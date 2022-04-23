cc0124b9a328e1a3213c2e64f87d57e0263873dc
hcoelho <coelhohernani09@gmail.com>
2022-04-23 21:20:05 +0000

    funcionalidade 1 completa

--- efolioA/readme.txt
+++ efolioA/readme.txt
@@ -3,20 +3,21 @@
 **************
 
 O programa foi desenvolvido em linguagem OCaml com a versão 4.11.1 e
-testado no sistema operativo Fedora 33 (linux)
+testado no sistema operativo Ubuntu (linux)
 
 O método de instalação utilizado é o descrito na página oficial do Ocaml
 ( https://ocaml.org/docs/install.html ) para a versão de SO referida anteriormente
 
-$  dnf install ocaml
+$  apt install ocaml
 
 **************
 * Compilação *
 **************
 
-Para compilar o programa basta escrever na linha de comandos o seguinte comando:
+Para compilar o programa basta escrever na linha de comandos o seguinte comando, a estrutura dos
+ficheiros não deverá ser alterada:
 
-$  ocamlopt -o snsidoso snsidoso.ml
+$  ocamlopt -o receitas receitas.ml
 
 **************
 *  Execução  *
@@ -24,7 +25,9 @@ $  ocamlopt -o snsidoso snsidoso.ml
 
 Para executar o programa basta escrever na linha de comandos o seguinte comando:
 
-$  ./snsidoso
+$  ./receitas
 
-Após o programa iniciar será solicitádo o nome do ficheiro que deverá ser lido.
-O ficheiro deverá estar estruturado da mesma forma que o ficheiro de exemplo "dados.txt"
+Após o programa iniciar, carrega para a memoria uma lista de registos com a informação 
+do ficheiro localizado em /dados/receitasV1.csv.
+Será apresentado um menu com a informação suscinta das receitas carregadas, bem como as opções disponiveis
+ao utilizador.
--- efolioA/receitas.ml
+++ efolioA/receitas.ml
@@ -4,12 +4,11 @@
  * Data: 20/04/2022
  * versão 1.0
 *)
-open Str
 
-(*Indicação nome do ficheiro para ler*)
+(*Indicação do diretorio do ficheiro de entrada*)
 let ficheiro ="dados/receitasV1.csv";;
 
-(*Estrutura de Dados de um Registo*)
+(*Estrutura de Dados de um Registo de receita*)
 type registo ={
   mutable numero: string;
   mutable nome: string;
@@ -38,19 +37,12 @@ type registo ={
   mutable qtdIng11: string;
 }
 
-(*Obtém o valor em formato string, que se encontra na cabelça da lista recebida 
-- Retorna String do facto - caso receba algo
-- String vazia - caso lista vazia *)
-let obterValorString lista = (
-  match lista with
-  | [] -> " "
-  | h::t -> h
-);;
-
- (*Separa os elementos da linha *)
-let separar linha = (   
-  let tokens = (String.split_on_char ';' linha) in   
-    {                                                  (* Separa os elementos da linah pelo "\t" *)
+(*Preenche um registo de receita com a informação que recebe de uma linha
+-recebe uma string, separa pelo delimitador
+-retorna um registo de receita para adicionar na lista de registo*)
+let preencherRegisto linha = (   
+  let tokens = (String.split_on_char ';' linha) in   (* Separa os elementos da linah pelo ";"*)
+    {                                                (*Adiciona nos campos de registo conforme ordem da lista de tokens*)  
     numero = List.nth tokens 0;
     nome = List.nth tokens 1;
     preparacao = List.nth tokens 2;
@@ -80,24 +72,26 @@ let separar linha = (
 );;
 
 (*Função para ler o ficheiro
-- recebe string como argumento
+- recebe string como argumento (localização do ficheiro)
 - retorna lista registos*)
 let lerFicheiro ficheiro : registo list =( 
-  if Sys.file_exists (ficheiro) then 
+  if Sys.file_exists (ficheiro) then                                      (*Verifica se ficheiro existe*)
     begin
-      let canal = open_in ficheiro in                                 (*Abrir o ficheiro*)
-      try let ler () = 
-        try Some (input_line canal) with End_of_file ->None in
-        let rec lerLinha lista = 
-          match ler () with 
-          | Some linha -> lerLinha(separar linha ::lista)
-          | None -> close_in_noerr canal; List.rev lista in lerLinha[]
-      with e -> close_in_noerr canal; []
+      let canal = open_in ficheiro in                                     (*Abrir o ficheiro, para ler*)               
+        try let ler () = 
+          try Some (input_line canal) with End_of_file ->None in          
+          let rec lerLinha lista =                                        (*Lê linha a linha recursivamente, recebe e retorna uma lista de registos*)
+            match ler () with                                             (*pattern matching*)
+            | Some linha -> lerLinha(preencherRegisto linha ::lista)      (*se houver alguma coisa, adiciona registo à lista*)
+            | None -> close_in_noerr canal; List.rev lista in lerLinha[]  (*Quando acabar, fecha canal, inverte lista*)
+        with e -> close_in_noerr canal; []                                (*caso não consiga levanta excepção e fecha canal*)
     end
   else []
 );;
 
-
+(*Função para imprimir informação de um registo
+- recebe um registo
+- não retorna nada (unit)*)
 let imprimirRegisto registo = (
   print_endline (String.make 75 '-');  
   Printf.printf "|  Receita %s: %-60s|\n" registo.numero registo.nome;
@@ -124,50 +118,78 @@ let imprimirRegisto registo = (
   print_endline (String.make 75 '-');
   Printf.printf "| 10: %-25s | %-39s |\n" registo.ingrediente10 registo.qtdIng10;
   print_endline (String.make 75 '-');
-  Printf.printf "| 11: %25s | %-39s |\n" registo.ingrediente11 registo.qtdIng11;
+  Printf.printf "| 11: %-25s | %-39s |\n" registo.ingrediente11 registo.qtdIng11;
   print_endline (String.make 75 '-');
-  Printf.printf "\nModo de Preparação:\n%s\n" registo.preparacao;
+  Printf.printf "\nModo de Preparação:\n%s\n\n" registo.preparacao;
 );;
 
-
-
-let rec obterRegisto lista index =(
-  match lista with
-  | [] -> raise (Failure "Lista esta vazia")
-  | first::rest -> 
-      if index = 1 then first 
-      else obterRegisto rest (index-1)
+(*Função que procura na lista o registo que se pretende
+- recebe lista de registos e informação que nome/numero que identifica registo
+- retorna o registo pretendido*)
+let obterRegisto lista indice =(
+  List.find (fun registo -> registo.numero = indice || registo.nome = indice) lista; (*Procura na lista o registo pelo nome ou numero*)
 );;
 
+(*Função recursiva que imprime no menu inicial as receitas disponiveis para analisar
+- recebe lista de registos
+- não retorna nada*)
 let rec imprimirMenuReceitas lista =( 
   match lista with
   | [] -> print_endline (String.make 38 '-');Printf.printf("Estas são as receitas diponiveis de momento!\n\n\n");
   | h::t -> print_endline (String.make 38 '-'); (Printf.printf "| %2s: %-30s |\n" h.numero h.nome); imprimirMenuReceitas t;
-) 
+);; 
 
-(*Função que apresenta menu e corre as funcionalidades*)
-let menu = (
-  let lRegistos = List.tl(lerFicheiro ficheiro) in        (*nova lista filtrada, para não incluir o primeiro registo (1º linha do file) da lista que recebe da funcao lerFicheiro*)
-  let terminaCiclo = ref false in
-  while not !terminaCiclo do
-    ignore(Sys.command "clear");
-    Printf.printf ("\n*****Bem Vindo ao Mundo das Receitas*****\n\n");
-    imprimirMenuReceitas lRegistos;  
-    Printf.printf ("Indique a opção que pretende:\n\t1 - Visualizar uma receita\n\t2 - Visualizar ementa do chefe\n\t0 - Sair\nOpção: ");
-    let controlo = read_int() in
-    if controlo = 1 then (
-      Printf.printf ("\nIndique o numero da receita que pretende visualizar: ");
-      let indice = read_int() in
-      let receita = obterRegisto lRegistos indice in
-        imprimirRegisto receita;
-      )
-    else if controlo = 2 then Printf.printf "Estamos a trabalhar na funcionalidade 2\n"
-    else terminaCiclo := true;
-    Printf.printf"\nPressione uma tecla para Continuar\n";
-    let voltar = read_line() in Printf.printf "%s" voltar;
-  done
+(*Função que mediante as receitas selecionadas cria uma nova lista só com esses registos
+- recebe uma string com identificação das escolhas
+- retorna uma lista registos com os 3 registos*)
+let compilarRegistos selecao lista =(
+  let ldiaria : registo list = [] in                  (*lista de registos vazia*)
+  let tokens = (String.split_on_char ',' selecao) in  (*separa argumentos pela virgula*)
+    let rec compilar tokens ldiaria =(                (*recursivamente preenche a nova lista de registos com base nas escolhas do utilizador*)
+      match tokens with
+      | [] -> List.rev ldiaria                        (*quando processar a lista de tokens, devolve lista com 3 receitas*)
+      | h::t -> compilar t (obterRegisto lista h::ldiaria)
+    )in compilar tokens ldiaria 
 );;
 
+(*Função para processar ementa diaria compilando total de igredientes que são iguais
+Nota: Função está incompleta, faltou fazer a compilação de ingredientes.
+- recebe lista receitas diaria
+- não retorna nada*)
+let rec processarSelecaoDiaria lista =(
+  match lista with
+  | [] -> ()                                              (*quando acabar, não retorna nada, unit*)
+  | h::t -> imprimirRegisto h; processarSelecaoDiaria t; 
+);;
+
+(*Função que apresenta menu e corre as funcionalidades
+- do tipo unit*)
+let menu = (
+  let lRegistos = List.tl(lerFicheiro ficheiro) in            (*nova lista filtrada, para não incluir o primeiro registo (1º linha do file) da lista que recebe da funcao lerFicheiro*)
+  let terminaCiclo = ref false in                     
+    while not !terminaCiclo do                                (*ciclo while para ciclicamente apresentar interface menu ao utilizador, com variavel de controlo*)
+      ignore(Sys.command "clear");                            (*limpa tela*)
+      Printf.printf ("\n*****Bem Vindo ao Mundo das Receitas*****\n\n");  
+      imprimirMenuReceitas lRegistos;                         (*Imprime resumo das receitas disponiveis*)
+      Printf.printf ("Indique a opção que pretende:\n\t1 - Visualizar uma receita\n\t2 - Escolha diária do chef em número de três receitas\n\t0 - Sair\nOpção: ");
+      let controlo = read_int() in                            (*utilizador indica opcao, para o levar à funcionalidade pretendida*)
+        if controlo = 1 then (                        
+          Printf.printf ("\nIndique o numero da receita que pretende visualizar: ");
+          let indice = read_line() in
+            let receita = obterRegisto lRegistos indice in    (*Obtém a receita e imprime de seguida a sua informação*)
+              imprimirRegisto receita;    
+          )
+        else if controlo = 2 then (                           (*Funcionalidade não totalmente desenvolvida, ficou faltar processar para compilar informação entre receitas*)
+          Printf.printf ("\nIndique separado por [,] três receitas: ");
+          let selecao = read_line() in
+            let ldiaria = compilarRegistos selecao lRegistos in   (*lista de registo da escolha diaria do chefe, manda para compilar informação*)
+              processarSelecaoDiaria ldiaria;
+          )
+        else terminaCiclo := true;                            (*Qualquer outra tecla faz terminar ciclo e posteriormente o programa*)
+      Printf.printf"\nPressione uma tecla para Continuar\n";
+      let voltar = read_line() in ignore(voltar);             (*Permite utilizador controlar quando quer avançar no programa*)
+    done
+);;
 
 (*Inicio do Programa*)
 let main() = (
